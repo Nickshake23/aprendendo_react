b8831b16fbe00899c0f9f3311f385437a91191d2
DEV <DEV@NSN1191261181.edu119.local>
2026-02-11 14:55:59 -0300

    comentario

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <main>
+      <h1>Bem-vindo</h1>
+      <p>Entre com seus dados para acessar o sistema</p>
+
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Senha" />
+
+      <button>Entrar</button>
+    </main>
+  );
+}
